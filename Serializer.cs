6f5437440e1b084ab00ba0707e6f2530908d3baa
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2024-01-07 11:41:01 -0800

    Initial commit

---
+++ Serializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Jvu.Test.SharedLib.TsDeps
+{
+  public class Serializer
+  {
+    public string Serialize(object obj)
+    {
+      return JsonConvert.SerializeObject(obj);
+    }
+
+    public T Deserialize<T>(string jsonString)
+    {
+      return JsonConvert.DeserializeObject<T>(jsonString);
+    }
+  }
+}
